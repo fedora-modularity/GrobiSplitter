d37e94cb5113819b53636e1a434d401ab1d0ec33
Stephen Smoogen <smooge@gmail.com>
2019-05-17 14:57:30 -0400

    This commit mostly moves items around to make more logical sense for myself. I am putting things in a linear order of how they are called and putting in comments in the code to better understand things.
    Signed-off-by: Stephen Smoogen <smooge@gmail.com>

--- splitter.py
+++ splitter.py
@@ -1,5 +1,7 @@
 #!/bin/python3
 
+# Import libraries needed for application to work
+
 import argparse
 import shutil
 import gi
@@ -11,12 +13,23 @@ import os
 import subprocess
 import sys
 
-gi.require_version('Modulemd', '2.0')
-from gi.repository import Modulemd
+# Look for a specific version of modulemd. The 1.x series does not
+# have the tools we need.
+try:
+    gi.require_version('Modulemd', '2.0')
+    from gi.repository import Modulemd
+except:
+    print("We require newer vesions of modulemd than installed..")
+    sys.exit(0)
+    
 mmd = Modulemd
 
-
 def _get_repoinfo(directory):
+    """
+    A function which goes into the given directory and sets up the
+    needed data for the repository using librepo.
+    Returns the LRR_YUM_REPO
+    """
     with tempfile.TemporaryDirectory(prefix='elsplit_librepo_') as lrodir:
         h = librepo.Handle()
         h.setopt(librepo.LRO_URLS, ["file://%s" % directory])
@@ -26,7 +39,6 @@ def _get_repoinfo(directory):
         h.setopt(librepo.LRO_IGNOREMISSING, False)
         r = h.perform()
         return r.getinfo(librepo.LRR_YUM_REPO)
-
 
 def _get_hawkey_sack(repo_info):
     hk_repo = hawkey.Repo("")
@@ -99,32 +111,6 @@ def validate_filenames(directory, repoinfo):
     return isok
 
 
-def parse_repository(directory):
-    """
-    Parse a specific directory, returning a dict with keys module NSVC's and
-    values a list of package NVRs.
-    The dict will also have a key "non_modular" for the non-modular packages.
-    """
-    directory = os.path.abspath(directory)
-    repo_info = _get_repoinfo(directory)
-
-    # Sometimes you get someone who blindly runs this against any
-    # repository they find.  Let them know this is meant to work only
-    # on regular repos.
-    if 'modules' not in repo_info:
-        print("This repository has no modules defined.")
-        print("Grobisplitter only works on repos with modules.")
-        sys.exit(0)
-
-    mod = _parse_repository_modular(repo_info)
-    modpkgset = _get_modular_pkgset(mod)
-    non_modular = _parse_repository_non_modular(repo_info, modpkgset)
-
-    mod['non_modular'] = non_modular
-
-    return mod
-
-
 def _perform_action(src, dst, action):
     if action == 'copy':
         try:
@@ -160,6 +146,10 @@ def create_repos(target, repos):
 
 
 def parse_args():
+    """
+    A standard argument parser routine which pulls in values from the
+    command line and returns a parsed argument dictionary.
+    """
     parser = argparse.ArgumentParser(description='Split repositories up')
     parser.add_argument('repository', help='The repository to split')
     parser.add_argument('--action', help='Method to create split repos files',
@@ -170,11 +160,18 @@ def parse_args():
                         action='store_true', default=False)
     parser.add_argument('--create-repos', help='Create repository metadatas',
                         action='store_true', default=False)
+    parser.add_argument('--default', help='Only create default and ursa repos',
+                        action='store_true', default=False)
     return parser.parse_args()
 
 
-def check_args(args):
+def setup_target(args):
+    """
+    Checks that the target directory exists and is empty. If not it
+    exits the program.  Returns nothing.
+    """
     if args.target:
+        args.target = os.path.abspath(args.target)
         if os.path.exists(args.target):
             if not os.path.isdir(args.target):
                 raise ValueError("Target must be a directory")
@@ -183,13 +180,41 @@ def check_args(args):
         else:
             os.mkdir(args.target)
 
+def parse_repository(directory):
+    """
+    Parse a specific directory, returning a dict with keys module NSVC's and
+    values a list of package NVRs.
+    The dict will also have a key "non_modular" for the non-modular packages.
+    """
+    directory = os.path.abspath(directory)
+    repo_info = _get_repoinfo(directory)
+
+    # Sometimes you get someone who blindly runs this against any
+    # repository they find.  Let them know this is meant to work only
+    # on repositories with modules.
+
+    if 'modules' not in repo_info:
+        print("This repository has no modules defined.")
+        print("Grobisplitter only works on repos with modules.")
+        sys.exit(0)
+
+    mod = _parse_repository_modular(repo_info)
+    modpkgset = _get_modular_pkgset(mod)
+    non_modular = _parse_repository_non_modular(repo_info, modpkgset)
+
+    mod['non_modular'] = non_modular
+
+    return mod
 
 def main():
+    # Determine what the arguments are and 
     args = parse_args()
-    if args.target:
-        args.target = os.path.abspath(args.target)
-    check_args(args)
+
+    # Go through arguments and act on their values.
+    setup_target(args)
+
     repos = parse_repository(args.repository)
+
     if not args.skip_missing:
         if not validate_filenames(args.repository, repos):
             raise ValueError("Package files were missing!")
